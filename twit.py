b4be29189ce6e0e6765c9f12a488bb9acea333b3
Younggun Kim <scari.net@gmail.com>
2015-03-03 15:28:25 +0900

    Moved to Twython.

--- twit.py
+++ twit.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python2
-# coding: utf8
+#!/usr/bin/env python3
 
-import twitter
+from twython import Twython
 from local_setting import *
 
 TWITTER_ACCOUNT = {
-    'access_token_secret': SCARIBOT_ACCESS_SECRET,
-    'access_token_key': SCARIBOT_ACCESS_TOKEN,
-    'consumer_secret': SCARIBOT_CONSUMER_SECRET,
-    'consumer_key': SCARIBOT_CONSUMER_KEY
+    'oauth_token_secret': SCARIBOT_ACCESS_SECRET,
+    'oauth_token': SCARIBOT_ACCESS_TOKEN,
+    'app_secret': SCARIBOT_CONSUMER_SECRET,
+    'app_key': SCARIBOT_CONSUMER_KEY
 }
 MAX_LEN = 140
-t = twitter.Api(**TWITTER_ACCOUNT)
+t = Twython(**TWITTER_ACCOUNT)
 
 def dm(screen_name, message):
     if screen_name and message:
         if len(message) > MAX_LEN:
             head = message[:MAX_LEN]
             message = message[MAX_LEN:]
-            t.PostDirectMessage(screen_name=screen_name, text=head)
+            t.send_direct_message(screen_name=screen_name, text=head)
+        else:
+            t.send_direct_message(screen_name=screen_name, text=message)
+            return
 
-        return t.PostDirectMessage(screen_name=screen_name, text=message)
-
-#print(t.VerifyCredentials())
+        return dm(screen_name, message)
